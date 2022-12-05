2fafba66d765328e0547b44e855686205b57fded
Roy Salazar <roysalazarp@gmail.com>
2022-12-05 13:22:46 +0200

    renamed docker containers

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 docker compose -f docker-compose.build.yml build
 
 # Push builds to docker hub
-docker push roysalazarportilla/portfolio_entry_nginx:latest
+docker push roysalazarportilla/portfolio_entry_nginx-reverse-proxy:latest
 docker push roysalazarportilla/portfolio_entry_ui:latest
